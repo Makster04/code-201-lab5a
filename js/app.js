ee5aeb857798b1b2d3e89a96e6fa19b2ad4fbf1b
MAK TRNKA <maktrnka@comcast.net>
2024-01-28 22:00:01 -0800

    Add it to the main

--- js/app.js
+++ js/app.js
@@ -52,8 +52,8 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sumAndMultiply(a, b, c) { //eslint-disable-line
-    let sumNumber = a + b + c;
-    let multiplyNumber = a * b * c;
+    let sumNumber = a + b + c; // sumNumber = sum(sum(a, b), c)
+    let multiplyNumber = a * b * c; // multiplyNumber = multiply(multiply(a, b), c)
     return [sumNumber, multiplyNumber, `${a} and ${b} and ${c} sum to ${sumNumber}.`, `The product of ${a} and ${b} and ${c} is ${multiplyNumber}.`]
 }
 
@@ -74,14 +74,14 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 let testArray = [2, 3, 4]; //eslint-disable-line
-
 function sumArray(sumArr) { //eslint-disable-line
-
+    let sum = (6 / 2) * 3;
+    return [sum, `${testArray} was passed in as an array of numbers, and ${sum} is their sum.`];
 }
 
 // Here is the test for sumArray(); uncomment it to run it
 
-// testSumArray(testArray);
+testSumArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
